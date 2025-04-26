32c770a09b7431d57264cea99dbbeffc903fb359
Shifani16 <110917558+Shifani16@users.noreply.github.com>
2025-04-26 12:25:12 +0700

    Added Seeder for Some Tables

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $i) {
+            User::create([
+                'name' => $faker->name,
+                'nip' => $faker->unique()->numerify('##########'),
+                'nip_verified_at' => now(),
+                'password' => Hash::make('12345'),
+            ]);
+        }
+    }
+}
