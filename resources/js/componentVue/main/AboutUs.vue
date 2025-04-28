e7314dec7b9915b66146f013632b07774fa0c16b
unknown <aliyarizkif04@gmail.com>
2025-04-28 20:17:38 +0700

    Finished About Us

--- resources/js/componentVue/main/AboutUs.vue
+++ resources/js/componentVue/main/AboutUs.vue
@@ -1,20 +1,22 @@
 <template>
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"> -->
 
     <div class="aboutus">
         <header class="bg-[#CAEAF2] px-10 py-6 flex justify-between items-center">
             <div class="flex items-center gap-6">
-                <img src="@/assets/occuhelp-full-logo.svg" alt="OccuHelp Full Logo" class="h-14"/>
-                <nav class=" ml-60 space-x-6 font-bold text-black">
-                    <a href="#" class="hover:text-[#305879]">Dashboard</a>
-                    <a href="#" class="text-[#305879] border-b-2 border-sky-800">Tentang Kami</a>
-                    <a href="#" class="hover:text-[#305879]">Layanan Kami</a>
-                    <a href="#" class="hover:text-[#305879]">Kontak</a>
-                </nav>
+                <img src="@/assets/occuhelp-full-logo.svg" alt="OccuHelp Full Logo" class="h-15"/>
             </div>
-                <span class="h-10">
-                    <img src="@/assets/profileBlack.svg"/>
-                </span>
+                
+            <nav class="ml-auto hidden md:flex space-x-6 font-bold text-black">
+                <a href="#" class="hover:text-[#305879]">Dashboard</a>
+                <a href="#" class="text-[#305879] border-b-2 border-sky-800">Tentang Kami</a>
+                <a href="#" class="hover:text-[#305879]">Layanan Kami</a>
+                <a href="#" class="hover:text-[#305879]">Kontak</a>
+            </nav>
+
+            <span class="h-10 ml-15">
+                <img src="@/assets/profileBlack.svg" class="h-8"/>
+            </span>
         </header>
 
         <!-- About Us: Introduction -->
@@ -25,13 +27,13 @@
 
             <div class="relative z-20 flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto gap-10">
                 <div class="md:w-1/2 text-left">
-                    <h1 class=" -mt-15 text-5xl font-bold">
+                    <h1 class=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-8 -mt-6">
                         KESEHATAN ANDA ADALAH
                         <span class="text-[#217F96]">PRIORITAS</span>
                         KAMI
                     </h1>
 
-                    <p class="mt-8 mb-10 text-gray-700 text-2xl">
+                    <p class="text-sm sm:text-base md:text-lg lg:text-2xl text-gray-700">
                         Kami menyediakan layanan pengelolaan data hasil MCU secara aman dan 
                         efisien untuk membantu Anda memantau kesehatan dengan lebih mudah.
                     </p>
@@ -44,12 +46,12 @@
         </section>
 
         <!-- About Us: Apa itu OccuHelp -->
-         <section class="relative px-10 py-24 bg-white min-h-[50vh]">
+         <section class="relative mt-20 px-4 sm:px-1 py-10 sm:py-16 md:py-24 bg-white min-h-[50vh] flex items-center">
             <div class="absolute inset-0 flex justify-center items-center z-0">
                 <img src="@/assets/aboutus-occuhelp-bg.svg" alt="Background Apa Itu OccuHelp" class="max-h-[90%] object-contain"/>
             </div>
 
-            <div class="relative z-10 max-w-4xl mx-auto text-left px-60">
+            <div class="relative z-10 max-w-4xl mx-auto text-left px-4 sm:px-44 md:px-60">
                 <h2 class="text-4xl font-bold mb-6">Apa itu OccuHelp?</h2>
                 <p class="text-gray-700 text-justify">
                     OccuHelp adalah platform digital yang memudahkan tenaga kesehatan 
@@ -60,12 +62,12 @@
         </section>
 
         <section>
-            <div class="flex justify-center gap-10 mb-16 flex-wrap">
+            <div class="flex justify-center gap-10 mt-20 mb-30 flex-wrap">
                 <!-- Visi -->
-                <div class="bg-[#95D4E4] p-6 rounded-lg flex-1 min-w-[250px] max-w-sm">
-                    <img src="@/assets/visionBlack.svg" alt="Visi" class="mx-auto h-15 mb-5 mt-1" />
-                    <h3 class="text-center text-2xl font-bold mb-2">VISI</h3>
-                    <p class="text-gray-700 text-justify">
+                <div class="card hoverable-card group bg-[#95D4E4] p-8 rounded-lg flex-1 min-w-[250px] max-w-[450px] transition-all duration-300">
+                    <img src="@/assets/visionBlack.svg" alt="Visi" class="icon-img mx-auto h-15 mb-5 mt-1 transition-all duration-300" />
+                    <h3 class="text-center text-[24px] font-bold mb-2">VISI</h3>
+                    <p class="text-gray-700 text-justify text-[20px]">
                         Visi kami adalah mempermudah tenaga kesehatan mengelola data 
                         Medical Check-Up pekerja secara efisien dan akurat, serta mendukung 
                         kesehatan pekerja melalui sistem yang mudah diakses dan terpercaya.
@@ -73,10 +75,10 @@
                 </div>  
                 
                 <!-- Misi -->
-                <div class="bg-[#95D4E4] p-6 rounded-lg flex-1 min-w-[250px] max-w-sm">
-                    <img src="@/assets/missionBlack.svg" alt="Misi" class="mx-auto h-15 mb-5 mt-1" />
-                    <h3 class="text-center text-2xl font-bold mb-2">MISI</h3>
-                    <p class="text-gray-700 text-justify">
+                <div class="card hoverable-card group bg-[#95D4E4] p-8 rounded-lg flex-1 min-w-[250px] max-w-[450px] transition-all duration-300">
+                    <img src="@/assets/missionBlack.svg" alt="Misi" class="icon-img mx-auto h-15 mb-5 mt-1 transition-all duration-300" />
+                    <h3 class="text-center text-[24px] font-bold mb-2">MISI</h3>
+                    <p class="text-gray-700 text-justify text-[20px]">
                         Misi kami adalah menyediakan platform teknologi untuk memudahkan 
                         tenaga medis mengelola data kesehatan pekerja, mengurangi beban 
                         administratif, dan meningkatkan akurasi pengelolaan data.
@@ -84,10 +86,12 @@
                 </div>  
             </div>
 
-            <div class="flex items-center justify-center gap-10 bg-[#E6F6F9] p-10 rounded-xl max-w-5xl mx-auto">
-                <img src="@/assets/aboutUsPerson.svg" alt="About Us Person" class="w-60 rounded-xl"/>
-                <div class="text-left">
-                    <h3 class="text-3xl font-bold mb-4">Kenapa harus OccuHelp?</h3>
+            <div class="relative bg-[#E6F6F9] p-10 rounded-xl max-w-5xl mx-auto h-min-h-[200px] sm:min-h-[350px] md:min-h-[150px] flex items-center justify-center gap-10 overflow-visible">
+                <img src="@/assets/aboutUsPerson.svg" alt="About Us Person" 
+                    class="absolute mr-150 -top-10 sm:-top-20 h-[300px] sm:h-[400px] md:h-[500px] lg:h-[430px] object-contain rounded-xl"
+                />
+                <div class="ml-90 text-left">
+                    <h3 class="text-[48px] font-bold mb-4">Kenapa harus OccuHelp?</h3>
                     <p class="text-gray-700 text-justify">
                         OccuHelp mengatasi tantangan pengelolaan data kesehatan pekerja 
                         dengan solusi teknologi praktis, memungkinkan tenaga medis fokus 
@@ -97,23 +101,25 @@
             </div>
         </section>
 
-         <footer class="bg-[#78C8DD] px-10 py-10 mt-20">
+        <footer class="bg-[#78C8DD] px-10 py-10 mt-20 rounded-tl-4xl rounded-tr-4xl">
             <div class="flex justify-between flex-wrap gap-8">
                 <div>
-                    <h4 class="font-bold mb-2">Butuh Bantuan?</h4>
+                    <h4 class="font-bold mb-2 text-[24px]">Butuh Bantuan?</h4>
                     <p class="mb-3">
                         Kami siap membantu! Jika ada pertanyaan atau butuh informasi lebih 
                         lanjut, hubungi tim kami untuk solusi terbaik.
                     </p>
-                    <p class="text-[#3C646F] font-bold mb-2">
-                        Email
-                    </p>
-                    <img src="@/assets/footer-line.svg">
-                    <input>
+                    <div class="flex items-center gap-2 mb-2">
+                        <input type="email" placeholder="Email"
+                        class="text-[#3C646F] font-bold mt-2 p-2 rounded-md w-full max-w-xs focus:outline-none"/>
+                        <img src="@/assets/arrow-right.svg" alt="Arrow" class="mt-3 h-4" />
+                    </div>
+                    
+                    <img src="@/assets/footer-line.svg" class="w-89">
                 </div>
 
                 <div>
-                    <h4 class="font-bold mb-2">Halaman</h4>
+                    <h4 class="font-bold mb-2 text-[20px]">Halaman</h4>
                     <ul class="space-y-1 text-gray-800">
                         <li>Dashboard</li>
                         <li>Tentang Kami</li>
@@ -122,7 +128,7 @@
                 </div>
 
                 <div>
-                    <h4 class="font-bold mb-2">Dukungan</h4>
+                    <h4 class="font-bold mb-2 text-[20px]">Dukungan</h4>
                     <ul class="space-y-1 text-gray-800">
                         <li>Hubungi Kami</li>
                         <li>Pusat Dukungan</li>
@@ -131,18 +137,54 @@
                 </div>
 
                 <div>
-                    <h4 class="font-bold mb-2">Mitra</h4>
+                    <h4 class="font-bold mb-2 text-[20px]">Mitra</h4>
                     <ul class="space-y-1 text-gray-800">
                         <li>Mitra Kami</li>
                         <li>Komunitas</li>
                         <li>Investor</li>
                     </ul>
                 </div>
-
             </div>
-         </footer>
+
+            <div class="relative bg-[#CAEAF2] rounded-3xl mt-8 px-10 py-4">
+                <div class="relative flex flex-wrap justify-between items-center max-w-7xl mx-auto">
+                    <!-- Bottom Left -->
+                    <img src="@/assets/occuhelp-full-logo.svg" alt="OccuHelp Logo" class="h-12"/>
+
+                    <!-- Bottom Center -->
+                    <p class="text-xs sm:text-sm md:text-base lg:text-lg font-bold text-center">
+                        © 2025 OccuHelp. All rights reserved.
+                    </p>
+
+                    <!-- Bottom Right -->
+                     <div class="flex gap-4">
+                        <img src="@/assets/facebook.svg" alt="Facebook"
+                            class="h-5 w-5 transition-transform transform hover:scale-125 cursor-pointer hover:content-[url('@/assets/facebook-hover.svg')]"/>
+                        <img src="@/assets/twitter.svg" alt="Twitter"
+                            class="h-5 w-5 transition-transform transform hover:scale-125 cursor-pointer hover:content-[url('@/assets/twitter-hover.svg')]"/>
+                        <img src="@/assets/insta.svg" alt="Instagram"
+                            class="h-5 w-5 transition-transform transform hover:scale-125 cursor-pointer hover:content-[url('@/assets/insta-hover.svg')]"/>
+                        <img src="@/assets/whatsapp.svg" alt="Whatsapp"
+                            class="h-5 w-5 transition-transform transform hover:scale-125 cursor-pointer hover:content-[url('@/assets/whatsapp-hover.svg')]"/>
+                     </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </template>
 
 <script setup></script>
-<style scoped></style>
+
+<style scoped>
+.hoverable-card:hover {
+  background: linear-gradient(to bottom, #CAEAF2, #95D4E4);
+}
+
+.hoverable-card:hover .icon-img[alt="Visi"] {
+  content: url("@/assets/visionHover.svg");
+}
+
+.hoverable-card:hover .icon-img[alt="Misi"] {
+  content: url("@/assets/missionHover.svg");
+}
+</style>
