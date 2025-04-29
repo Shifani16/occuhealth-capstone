be8343b8fb147022794ab179d4f141565db99dc0
Shifani16 <110917558+Shifani16@users.noreply.github.com>
2025-04-29 07:49:18 +0700

    Finished Reset Password Logic (3)

--- routes/web.php
+++ routes/web.php
@@ -4,31 +4,16 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ForgotPasswordController;
 
-
 Route::get('/', function () {
     return redirect('/login');
 });
 
+// Important route with signed middleware
 Route::get('/verify-reset/{user}', [ForgotPasswordController::class, 'verifyLink'])
     ->name('password.reset')
     ->middleware('signed');
 
+// Catch-all, but EXCLUDING verify-reset
 Route::get('/{any}', function () {
     return view('app');
-})->where('any', '.*');
-
-// Route::get('/login', function () {
-//     return view('auth.login');
-// });
-
-// Route::get('/forgotpass', function () {
-//     return view('auth.forgotpass');
-// });
-
-// Route::get('/verifypass', function () {
-//     return view('auth.verifypass');
-// });
-
-// Route::get('/aboutus', function () {
-//     return view('main.aboutus');
-// });
+})->where('any', '^(?!verify-reset).*$'); 
